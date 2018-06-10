ea2ab376bcec979eb214754d0e6262a3182164aa
puxiang@shuangchuangpuhui.com <puxiang@shuangchuangpuhui.com>
2018-06-10 16:11:27 +0800

    修改返回值

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,7 +9,7 @@ struct LoginRequest: Content {
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
     router.get("hello") { req in
-        return "Hello, world!"
+        return ["message": "Hello, world!"]
     }
 
     // Example of configuring a controller
